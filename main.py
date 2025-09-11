d2f6724844612e7523722ca6811a8ed40a51bbaa
Lil_Drew <37800669+cozy24@users.noreply.github.com>
2025-09-12 01:52:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 from sklearn.metrics import f1_score, recall_score, roc_auc_score, precision_score, accuracy_score
 import numpy as np
 import torch
-import os
+from model import CombinedModel
 from sklearn.model_selection import train_test_split
 from dataset import Dataset  # 假设Dataset类用于加载数据和构建图
 from utils import *
@@ -23,16 +23,6 @@ def get_best_f1(labels, probs):
             best_thre = thres
     return best_f1, best_thre
 
-# def get_best_f1_edge(labels, probs):
-#     best_f1, best_thre = 0, 0
-#     for thres in np.linspace(0.05, 0.95, 19):
-#         preds = np.zeros_like(labels)
-#         preds = (probs >= thres).astype(int)
-#         mf1 = f1_score(labels, preds, average='macro')
-#         if mf1 > best_f1:
-#             best_f1 = mf1
-#             best_thre = thres
-#     return best_f1, best_thre
 
 def train(model, graph, args, device, quantile):
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-5)
@@ -49,12 +39,17 @@ def train(model, graph, args, device, quantile):
     patience = args.patience  # Early Stopping 的耐心值
     best_model_state = None  # 保存最佳模型的状态
     time_start = time.time()
+    train_times = []
+    inference_times = []
+    train_memories = []
+    inference_memories = []
 
     for epoch in range(args.epoch):
+
         model.train()
         optimizer.zero_grad()
 
-        output, edge_pred, reconstruction_loss, _ = model(graph, features)
+        output, edge_pred, reconstruction_loss = model(graph, features)
         total_loss = 0
 
         # 处理边标签和掩码
@@ -74,12 +69,13 @@ def train(model, graph, args, device, quantile):
 
         total_loss.backward()
         optimizer.step()
-        torch.cuda.empty_cache()
 
-        # 验证阶段
+
+        # === 推理阶段（验证）===
         model.eval()
         with torch.no_grad():
-            output, edge_pred, reconstruction_loss, combined_out = model(graph, features)
+            output, edge_pred, reconstruction_loss = model(graph, features)
+            
             if isinstance(graph.edata['label'], dict):
                 edge_labels = {etype: label.to(device) for etype, label in graph.edata['label'].items()}
             else:
@@ -91,6 +87,7 @@ def train(model, graph, args, device, quantile):
                 edge_test_mask = graph.edata['edge_train_mask'].to(device).bool()
 
             val_loss = model.compute_loss(output, edge_pred, labels, edge_labels, test_mask, edge_test_mask, reconstruction_loss)
+
             val_losses.append(val_loss.item())
 
             probs = F.softmax(output, dim=1)
@@ -130,94 +127,42 @@ def train(model, graph, args, device, quantile):
                 print(f"Test Loss {val_loss.item()}")
                 print('Epoch {}, loss: {:.4f}, val mf1: {:.4f}, (best {:.4f})'.format(epoch, val_loss.item(), f1, best_f1))
 
-    time_end = time.time()
-    print('time cost: ', time_end - time_start, 's')
-
     print('Test: REC {:.2f} PRE {:.2f} MF1 {:.2f} AUC {:.2f} G-Mean {:.2f}'.format(
         final_trec * 100, final_tpre * 100, final_tmf1 * 100, final_tauc * 100, final_gmean * 100))
-    # # 获取测试集的节点原始特征、最后的节点表征和标签
-    test_nodes = graph.ndata['feature'][test_mask].cpu().numpy()  # 测试集节点的原始特征
-    final_embeddings = combined_out[test_mask].cpu().detach().numpy()  # 最后的节点表征
-    test_labels = graph.ndata['label'][test_mask].cpu().numpy()  # 测试集节点的标签
 
-    # 创建 tsne 文件夹（如果不存在）
-    tsne_folder = 'tsne'
-    if not os.path.exists(tsne_folder):
-        os.makedirs(tsne_folder)
-    # 构建文件名
-    test_nodes_filename = os.path.join(tsne_folder, f"{args.dataset}_test_nodes_features.npy")
-    final_embeddings_filename = os.path.join(tsne_folder, f"{args.dataset}_final_node_embeddings.npy")
-    test_labels_filename = os.path.join(tsne_folder, f"{args.dataset}_test_nodes_labels.npy")
-    # 保存数据
-    np.save(test_nodes_filename, test_nodes)
-    np.save(final_embeddings_filename, final_embeddings)
-    np.save(test_labels_filename, test_labels)
+    return final_trec, final_tpre, final_tmf1, final_tauc, final_gmean, train_times, inference_times, train_memories, inference_memories
 
 
-    # # 加载最佳模型状态
-    # if best_model_state is not None:
-    #     model.load_state_dict(best_model_state)
-    #     print("Loaded best model state.")
-
-    return final_trec, final_tpre, final_tmf1, final_tauc, final_gmean
 
 def StartTrain(in_feats, h_feats, out_feats, num_classes, graph, d, pos_quantile, neg_quantile):
 
     print('train/test samples: ', train_mask.sum().item(), test_mask.sum().item())
     labels = graph.ndata['label'].to(device)
     weight = (1 - labels[train_mask]).sum().item() / labels[train_mask].sum().item()
-    print('cross entropy weight: ', weight)
     edge_weight = 0
     print("Starting training")
     # 遍历模型参数并打印每个参数的维度
     # for name, param in model.named_parameters():
     #     print(f"Parameter name: {name}, Parameter shape: {param.shape}")
-    if args.type == 'full':
-        from model import CombinedModel
-    elif args.type == 'without_attention':
-        from model_without_attention import CombinedModel
-    elif args.type == 'without_partition':
-        from model_without_partition import CombinedModel
-    elif args.type == 'without_reconstruction':
-        from model_without_reconstruction import CombinedModel
     model = CombinedModel(in_feats, h_feats, out_feats, num_classes, graph, d=d, pos_quantile=pos_quantile, neg_quantile=neg_quantile, num_heads=args.num_heads, mode=args.mode)
     model = model.to(device)
     return model, weight, edge_weight
 
-def compute_gmean(labels_np, preds_np):
-    from sklearn.metrics import confusion_matrix
-    # 计算混淆矩阵
-    tn, fp, fn, tp = confusion_matrix(labels_np, preds_np).ravel()
-    # 计算 Sensitivity (召回率)
-    if (tp + fn == 0):
-        sensitivity = 0
-    else:
-        sensitivity = tp / (tp + fn)
-    # 计算 Specificity (特异度)
-    if (tn + fp == 0):
-        specificity = 0
-    else:
-        specificity = tn / (tn + fp)
-    # 计算 G-Mean
-    gmean = np.sqrt(sensitivity * specificity)
-    return gmean
-
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, default='yelp')
+    parser.add_argument('--dataset', type=str, default='amazon')
     parser.add_argument('--epoch', type=int, default=1000)
     parser.add_argument('--patience', type=int, default=100)
     parser.add_argument('--hid_dim', type=int, default=64)
     parser.add_argument('--homo', type=int, default=1)
     parser.add_argument('--order', type=int, default=2)
-    parser.add_argument('--run', type=int, default=10)
+    parser.add_argument('--run', type=int, default=1)
     parser.add_argument('--train_ratio', type=float, default=0.4)
     parser.add_argument('--pos_quantile', type=float, default=0.3)
     parser.add_argument('--neg_quantile', type=float, default=0.3)
     parser.add_argument('--num_heads', type=int, default=1)
     parser.add_argument('--mode', type=str, default='homo')
-    parser.add_argument('--type', type=str, default='full')
     parser.add_argument('--gpu', type=int, default=0,
                         help='Choose a specific CUDA device number, like 0, 1, etc.',
                         # 根据 torch.cuda.device_count() 生成合法的整数范围选项
@@ -337,10 +282,14 @@ if __name__ == '__main__':
     
 
     final_recs, final_pres, final_mf1s, final_aucs, final_gmeans = [], [], [], [], []
+    all_train_times = []
+    all_inference_times = []
+    all_train_memories = []
+    all_inference_memories = []
     for tt in range(args.run):
         print(f"Running {tt+1}/{args.run}...") 
         model, weight, edge_weight = StartTrain(in_feats, h_feats, out_feats, num_classes, graph, d, pos_quantile, neg_quantile)
-        rec, pre, mf1, auc, gmean = train(model, graph, args, device, edge_weight)
+        rec, pre, mf1, auc, gmean, train_times, inference_times, train_memories, inference_memories = train(model, graph, args, device, edge_weight)
         final_recs.append(rec)
         final_pres.append(pre)
         final_mf1s.append(mf1)
@@ -351,6 +300,11 @@ if __name__ == '__main__':
     final_mf1s = np.array(final_mf1s)
     final_aucs = np.array(final_aucs)
     final_gmes = np.array(final_gmeans)
+    # 累积资源信息
+    all_train_times.extend(train_times)
+    all_inference_times.extend(inference_times)
+    all_train_memories.extend(train_memories)
+    all_inference_memories.extend(inference_memories)
 
     print('-' * 60)  # 输出一条分隔线
     print('Test Results:'.center(60))  # 输出标题并居中
